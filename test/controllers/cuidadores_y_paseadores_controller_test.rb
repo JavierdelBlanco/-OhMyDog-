ec8c258c3f71ae317e0a95e815712e6e63217831
Javisama <javierdelblanco01@gmail.com>
2023-10-31 00:52:46 -0300

    ruby 3.2.2 complete

--- test/controllers/cuidadores_y_paseadores_controller_test.rb
+++
@@ -1,8 +0,0 @@
-require "test_helper"
-
-class CuidadoresYPaseadoresControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get cuidadores_y_paseadores_index_url
-    assert_response :success
-  end
-end
