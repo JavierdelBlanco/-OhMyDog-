1e954ce673c9bfa3846993454ed2ab084f6a797c
QueenNixu <agus.full_metal@yahoo.com.ar>
2023-11-08 23:08:30 -0300

    perros perdidos: no repetir nombre para el mismo usuario

--- app/models/perros_perdido.rb
+++ app/models/perros_perdido.rb
@@ -3,4 +3,13 @@ class PerrosPerdido < ApplicationRecord
     validates :nombre, presence: { message: "El nombre no puede estar en blanco" }
     validates :foto, presence: { message: "Debe subir una foto" }
 
+    
+    validate :nombre_unico_para_usuario_actual
+    
+    def nombre_unico_para_usuario_actual
+        if PerrosPerdido.exists?(nombre: nombre, mail: mail)
+            errors.add(:nombre, "Ya has publicado un perro con este nombre.")
+        end
+    end
+
 end
