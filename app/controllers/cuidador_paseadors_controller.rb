993899021a5abf5becbf82d3c84d600cdefb524a
Javisama <javierdelblanco01@gmail.com>
2023-11-10 12:54:37 -0300

    fix: alert

--- app/controllers/cuidador_paseadors_controller.rb
+++ app/controllers/cuidador_paseadors_controller.rb
@@ -25,7 +25,7 @@ class CuidadorPaseadorsController < ApplicationController
         format.html { redirect_to cuidador_paseadors_url, notice: "El cuidador/paseador fue cargado correctamente." }
         format.json { render :show, status: :created, location: @cuidador_paseador }
       else
-        flash[:notice] = "Falló la carga del cuidador/paseador."
+        flash[:alert] = "Falló la carga del cuidador/paseador."
         format.html { render :new, status: :unprocessable_entity }
         format.json { render json: @cuidador_paseador.errors, status: :unprocessable_entity }
       end
@@ -39,7 +39,7 @@ class CuidadorPaseadorsController < ApplicationController
         format.html { redirect_to cuidador_paseadors_url, notice: "El cuidador/paseador fue editado correctamente." }
         format.json { render :show, status: :ok, location: @cuidador_paseador }
       else
-        flash[:notice] = "Falló la edicion del cuidador/paseador."
+        flash[:alert] = "Falló la edicion del cuidador/paseador."
         format.html { render :edit, status: :unprocessable_entity }
         format.json { render json: @cuidador_paseador.errors, status: :unprocessable_entity }
       end
