bd7a95acc6bd6a2304d2694e12499e249c0fc4ca
JoacoRodriguez9 <joaquirodri2000@gmail.com>
2023-11-10 12:15:48 -0300

    correciones-registro

--- app/models/perrito.rb
+++ app/models/perrito.rb
@@ -1,13 +1,10 @@
 class Perrito < ApplicationRecord
-<<<<<<< HEAD
   validates :nombre, presence: true
   validates :caracteristicas, presence: true
   validates :condiciones, presence: true
   validates :raza, presence: true
   validates :color, presence: true
   validates :tamaño, presence: true
-=======
->>>>>>> cc972f2e (correcion-usuario-registro)
 
   belongs_to :user
 end
