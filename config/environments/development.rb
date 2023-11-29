970d71aca279bee9bfc2aebca87d13a436d36611
QueenNixu <agus.full_metal@yahoo.com.ar>
2023-11-29 05:12:12 -0300

    end pa. end vet de guardia

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -68,13 +68,13 @@ Rails.application.configure do
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
 
-config.action_mailer.delivery_method = :smtp
-config.action_mailer.smtp_settings = {
-  :user_name => '173dd241287f14',
-  :password => 'b18993ff7729e5',
-  :address => 'sandbox.smtp.mailtrap.io',
-  :host => 'sandbox.smtp.mailtrap.io',
-  :port => '2525',
-  :authentication => :cram_md5
-}
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :user_name => '173dd241287f14',
+    :password => 'b18993ff7729e5',
+    :address => 'sandbox.smtp.mailtrap.io',
+    :host => 'sandbox.smtp.mailtrap.io',
+    :port => '2525',
+    :authentication => :cram_md5
+  }
 end
