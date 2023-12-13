c363a00294c717c25c7aaf5157d28e891c0455f6
JoacoRodriguez9 <joaquirodri2000@gmail.com>
2023-12-13 08:51:27 -0300

    scheduler-donaciones

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -70,8 +70,8 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    :user_name => 'b93006a29eb33e',
-    :password => '1e3288b5296f58',
+    :user_name => '0ecf18457e2983',
+    :password => '5948bbc05e056f',
     :address => 'sandbox.smtp.mailtrap.io',
     :host => 'sandbox.smtp.mailtrap.io',
     :port => '2525',
